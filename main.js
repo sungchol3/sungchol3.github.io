4d43edfecda97175592bcc596789d65842003329
sungchol3 <64838162+sungchol3@users.noreply.github.com>
2021-01-21 14:17:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -33,37 +33,10 @@ pianoElList.forEach(el => {
   el.addEventListener("transitionend", removeTransition);
 });
 
-
-var bStartEvent = false;
-//touchstart 이벤트 발생 여부 플래그
-var bMoveEvent = false;
-//touchmove 이벤트 발생 여부 플래그
- 
-
-document.addEventListener("touchstart", this.onStart.bind(this), false);
-document.addEventListener("touchmove", this.onMove.bind(this), false);
-document.addEventListener("touchend", this.onEnd.bind(this), false);
- 
-function onStart(e) {
-    bStartEvent = true;
-}
- 
-function onMove(e) {
-    
-    if(!bStartEvent) {
-        return;
-        //touchstart 이벤트가 발생하지 않으면 처리하지 않는다.
-    }
-    bMoveEvent = true;
-    //touchMove 이벤트 발생 여부를 설정한다.
-}
- 
-function onEnd(e) {
-    if(bStartEvent && !bMoveEvent) {
-        //클릭 이벤트로 판단한다.
-        alert(e);
-    }
-    //각 플래그 값을 초기값으로 설정한다.
-    bStartEvent = false;
-    bMoveEvent = false;
-}
+//Touch Event Handling
+document.querySelector('img').addEventListener('touchstart', function(ev) {
+  for (var index = 0; index < ev.targetTouches.length; index++) {
+    const element = ev.targetTouches.item(index);
+    alert(element);
+  }
+}, false);
