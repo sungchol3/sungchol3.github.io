9070f94bd67e76e992d54fbb4366fe60e821ffca
sungchol3 <64838162+sungchol3@users.noreply.github.com>
2021-01-21 13:53:00 +0900

    Update main.js
    I update main.js for test because I want to handle mobile touch event.

--- main.js
+++ main.js
@@ -31,3 +31,38 @@ const pianoElList = document.querySelectorAll("li");
 pianoElList.forEach(el => {
   el.addEventListener("transitionend", removeTransition);
 });
+
+
+var bStartEvent = false;
+//touchstart 이벤트 발생 여부 플래그
+var bMoveEvent = false;
+//touchmove 이벤트 발생 여부 플래그
+ 
+
+document.querySelector('button').addEventListener("touchstart", this.onStart.bind(this), false);
+document.querySelector('button').addEventListener("touchmove", this.onMove.bind(this), false);
+document.querySelector('button').addEventListener("touchend", this.onEnd.bind(this), false);
+ 
+function onStart(e) {
+    bStartEvent = true;
+}
+ 
+function onMove(e) {
+    
+    if(!bStartEvent) {
+        return;
+        //touchstart 이벤트가 발생하지 않으면 처리하지 않는다.
+    }
+    bMoveEvent = true;
+    //touchMove 이벤트 발생 여부를 설정한다.
+}
+ 
+function onEnd(e) {
+    if(bStartEvent && !bMoveEvent) {
+        //클릭 이벤트로 판단한다.
+        alert(e);
+    }
+    //각 플래그 값을 초기값으로 설정한다.
+    bStartEvent = false;
+    bMoveEvent = false;
+}
