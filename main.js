300c971c8833f09889d7c964fdfdba192ae26b60
sungchol3 <64838162+sungchol3@users.noreply.github.com>
2021-02-08 19:47:05 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
 playSound = e => {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = document.querySelector(`li[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`img[data-key="${e.keyCode}"]`);
   if (audio) {
     audio.currentTime = 0; // 처음으로 되돌리기
     audio.play();
     key.classList.add("playing");
+    /* setTimeout(function(){
+      key.classList.remove("playing");
+    }, 500); */
   }
 };
 
 function clickSound(arg){
-    const key = arg.parentNode;
+    const key = arg;
     var keyCode = key.getAttribute('data-key');
     const audio = document.querySelector('audio[data-key="'+keyCode+'"]');
     if (audio) {
         audio.currentTime = 0; // 처음으로 되돌리기
         audio.play();
-        key.classList.add("playing");
+        /* key.classList.add("playing");
+        setTimeout(function(){
+          key.classList.remove("playing");
+        }, 500); */
     }
 }
 
 removeTransition = e => {
-  if (e.propertyName === "transform") {
-    e.target.classList.remove("playing");
-  }
+  const key = document.querySelector(`img[data-key="${e.keyCode}"]`);
+  if (key != null) { key.classList.remove("playing");}
 };
 
 window.addEventListener("keydown", playSound);
+window.addEventListener("keyup", removeTransition);
 
-const pianoElList = document.querySelectorAll("li");
+/* const pianoElList = document.querySelectorAll("img");
 pianoElList.forEach(el => {
   el.addEventListener("transitionend", removeTransition);
-});
+}); */
 
 //Touch Event Handling
-document.querySelector('button').addEventListener('touchstart', function(ev) {
+/* document.querySelector('button').addEventListener('touchstart', function(ev) {
   ev.preventDefault();
   for (var index = 0; index < ev.targetTouches.length; index++) {
     const element = ev.target;
     alert(element);
   }
-}, false);
+}, false); */
